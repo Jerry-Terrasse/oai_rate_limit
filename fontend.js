6c87a531d528ddb71c821b194c9d727bb9d27408
Terrasse <terrasse@qq.com>
2024-10-27 14:35:07 +0800

    add display bar

--- fontend.js
+++ fontend.js
@@ -14,9 +14,24 @@
 (function() {
     'use strict';
 
-window.aaa = 1;
-var api_url = "https://oai-rl.terase.cn";
-var api_key = "<the_api_key>";
+window.model_status = {
+    "o1-preview": -1,
+    "o1-mini": -1,
+}
+
+function updateStatusText() {
+    var status = window.model_status;
+    var text = "";
+    for (const model in status) {
+        text += `${model}: ${status[model]}; `;
+    }
+    text = text.slice(0, -2);
+
+    var bar = document.getElementById("crl_bar");
+    if (bar) {
+        bar.innerText = text;
+    }
+}
 
 (function(fetch) {
     window.fetch = function(input, init) {
@@ -59,12 +74,52 @@ function receiveMessage(event) { // Accept: type="status"
     if (event.origin !== window.location.origin) return;
     if (event.data.type !== "status") return;
 
-    console.log('MAIN_WORLD 收到消息:', event.data);
-    // set page title
-    var title = document.querySelector('title');
-    title.textContent = `Rate Limit: ${event.data.remain}`;
+    var msg = event.data;
+    // console.log('MAIN_WORLD 收到消息:', msg);
+    var status = window.model_status;
+    if (msg.model in status) {
+        status[msg.model] = msg.remain;
+        updateStatusText();
+    } else {
+        console.log(`Unknown model: ${msg.model}`);
+    }
 }
 
 window.addEventListener('message', receiveMessage, false);
+
+function updateAll() {
+    console.log("Update All");
+    for (const model in window.model_status) {
+        window.postMessage({ model: model, type: "get" }, window.location.origin);
+    }
+}
+
+// Display & Refresh Button
+function htmlToNode(html) {
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+function addFrontendItems() {
+    var avatar = document.querySelector('button[data-testid="profile-button"]');
+    if (!avatar) return false;
+    var avatarContainer = avatar.parentElement;
+
+    var displayBar = htmlToNode('<div id="crl_bar">loading...</div>')
+    // var refreshButton = htmlToNode('<button onclick="updateAll();"><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"/></svg></button>')
+    
+    avatarContainer.prepend(displayBar);
+    return true;
+}
+function tryAddFrontendItems() {
+    if (addFrontendItems()) {
+        console.log("Frontend items added");
+        updateAll();
+        return;
+    }
+    setTimeout(tryAddFrontendItems, 200);
+}
+
+setTimeout(tryAddFrontendItems, 200);
 
 })();
